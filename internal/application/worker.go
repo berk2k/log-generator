f6b1e3c0cdd997ca4f48fcc0960da2ae0d065287
Berk Polat <berkpolat125@gmail.com>
2025-12-04 15:44:23 +0300

    added: producer

---
+++ internal/application/worker.go
@@ -0,0 +1,28 @@
+package application
+
+import (
+	"log-generator/internal/domain"
+	"log-generator/internal/infrastructure"
+)
+
+type Worker struct {
+	ID     int
+	InChan <-chan domain.LogMessage //recieve-only
+	Logger *infrastructure.ConsoleLogger
+}
+
+func newWorker(id int, in <-chan domain.LogMessage, logger *infrastructure.ConsoleLogger) *Worker {
+	return &Worker{
+		ID:     id,
+		InChan: in,
+		Logger: logger,
+	}
+}
+
+func (w *Worker) Start() {
+	go func() {
+		for msg := range w.InChan {
+			_ = w.Logger.Write(msg)
+		}
+	}()
+}
