78a1fa2667d3c61ccdce8c510aa08c77c574d2eb
Berk Polat <berkpolat125@gmail.com>
2025-12-11 17:55:51 +0300

    added: graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log-generator/internal/application"
 	"log-generator/internal/domain"
 	"log-generator/internal/infrastructure"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
 func main() {
+
+	// cancel context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// catch SIGINT(Ctrl + C)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
 	logChan := make(chan domain.LogMessage, 10000)
 
 	workerCount := 5
@@ -19,17 +32,32 @@ func main() {
 	logger := infrastructure.NewConsoleLogger()
 
 	// producer: for all 1ms 1 log = 1000 log/s
-	producer := application.NewProducer(logChan, time.Millisecond, metrics)
+	producer := application.NewProducer(logChan, time.Millisecond, metrics, ctx)
 	producer.Start()
+
+	// workers will be using WaitGroup
+	var wg sync.WaitGroup
+	wg.Add(workerCount)
 
 	// worker pool
 
 	for i := 1; i <= workerCount; i++ {
-		worker := application.NewWorker(i, logChan, logger, metrics)
+		worker := application.NewWorker(i, logChan, logger, metrics, &wg)
 		worker.Start()
 	}
 
-	time.Sleep(2 * time.Second) // run for 2 seconds
+	// wait for shutdown signal
+	<-sigChan
+	println("Shutdown signal received!")
+
+	// stop producer
+	cancel()
+
+	time.Sleep(200 * time.Millisecond) //wait for all producers to close before shutdown
+
+	close(logChan)
+
+	wg.Wait()
 
 	fmt.Println("Program finished")
 
