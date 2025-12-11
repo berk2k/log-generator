4f0d235b952648ff610e4cf452c38e2803398215
Berk Polat <berkpolat125@gmail.com>
2025-12-11 18:28:44 +0300

    updated: worker pool -> graceful

--- internal/application/worker.go
+++ internal/application/worker.go
@@ -3,6 +3,7 @@ package application
 import (
 	"log-generator/internal/domain"
 	"log-generator/internal/infrastructure"
+	"sync"
 )
 
 type Worker struct {
@@ -10,19 +11,23 @@ type Worker struct {
 	InChan  <-chan domain.LogMessage //recieve-only
 	Logger  *infrastructure.ConsoleLogger
 	Metrics *Metrics
+	Wg      *sync.WaitGroup
 }
 
-func NewWorker(id int, in <-chan domain.LogMessage, logger *infrastructure.ConsoleLogger, metrics *Metrics) *Worker {
+func NewWorker(id int, in <-chan domain.LogMessage, logger *infrastructure.ConsoleLogger, metrics *Metrics, wg *sync.WaitGroup) *Worker {
 	return &Worker{
 		ID:      id,
 		InChan:  in,
 		Logger:  logger,
 		Metrics: metrics,
+		Wg:      wg,
 	}
 }
 
 func (w *Worker) Start() {
 	go func() {
+		defer w.Wg.Done() // notify main
+
 		for msg := range w.InChan {
 			_ = w.Logger.Write(msg)
 
