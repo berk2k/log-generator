d692066dba7871c7166a50ad261e87dfa307cd12
Berk Polat <berkpolat125@gmail.com>
2025-12-13 17:02:56 +0300

    added: batch logic

--- internal/application/worker.go
+++ internal/application/worker.go
@@ -1,6 +1,7 @@
 package application
 
 import (
+	"fmt"
 	"log-generator/internal/domain"
 	"log-generator/internal/infrastructure"
 	"sync"
@@ -24,16 +25,37 @@ func NewWorker(id int, in <-chan domain.LogMessage, logger *infrastructure.Conso
 	}
 }
 
-func (w *Worker) Start() {
+func (w *Worker) Start(batchSize int) {
 	go func() {
-		defer w.Wg.Done() // notify main
+		defer w.Wg.Done()
+
+		// create batch slice
+		batch := make([]domain.LogMessage, 0, batchSize)
 
 		for msg := range w.InChan {
-			_ = w.Logger.Write(msg)
+
+			batch = append(batch, msg)
+
+			// if batch full --> FLUSH
+			if len(batch) >= batchSize {
+				w.processBatch(batch)
+
+				// Batch reset (no allocation!)
+				batch = batch[:0]
+			}
 
 			if w.Metrics != nil {
 				w.Metrics.IncProcessed()
 			}
 		}
+
+		// Worker is going down -> flush rest of the batch
+		if len(batch) > 0 {
+			w.processBatch(batch)
+		}
 	}()
+}
+
+func (w *Worker) processBatch(batch []domain.LogMessage) {
+	fmt.Printf("Worker %d flushing batch of %d logs\n", w.ID, len(batch))
 }
